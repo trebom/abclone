f2383ed28162bb74357c918299ad2358a4f4fab3
Your Name Here <shinykim63@gmail.com>
2020-12-13 14:39:23 -0500

    19.4

--- users/models.py
+++ users/models.py
@@ -36,6 +36,15 @@ class User(AbstractUser):
         (CURRENCY_USD, "USD"),
         (CURRENCY_KRW, "KRW"),
     )
+    LOGIN_EMAIL = "email"
+    LOGIN_GITHUB = "github"
+    LOGIN_KAKAO = "kakao"
+
+    LOGIN_CHOICES = (
+        (LOGIN_EMAIL, "email"),
+        (LOGIN_GITHUB, "github"),
+        (LOGIN_KAKAO, "kakao"),
+    )
 
     avatar = models.ImageField(upload_to="avatars", blank=True)
     gender = models.CharField(
@@ -60,6 +69,9 @@ class User(AbstractUser):
     superhost = models.BooleanField(default=False)
     email_verified = models.BooleanField(default=False)
     email_secret = models.CharField(max_length=20, default="", blank=True)
+    login_method = models.CharField(
+        max_length=50, choices=LOGIN_CHOICES, default=LOGIN_EMAIL
+    )
 
     def verify_email(self, email):
         if self.email_verified is False:
