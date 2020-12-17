3209d957f268951f18234b622da231f5d64fd795
Your Name Here <shinykim63@gmail.com>
2020-12-16 21:56:27 -0500

    24.18

--- reviews/models.py
+++ reviews/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-
-# Create your models here.
+from django.core.validators import MinValueValidator, MaxValueValidator
 from core import models as core_models
 
 # Create your models here.
@@ -11,12 +10,22 @@ class Review(core_models.TimeStampedModel):
     """ Review Model Definition """
 
     review = models.TextField()
-    accuracy = models.IntegerField()
-    communication = models.IntegerField()
-    cleanliness = models.IntegerField()
-    location = models.IntegerField()
-    check_in = models.IntegerField()
-    value = models.IntegerField()
+    accuracy = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    communication = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    cleanliness = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    location = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    check_in = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    value = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     user = models.ForeignKey(
         "users.User", related_name="reviews", on_delete=models.CASCADE
     )
@@ -37,5 +46,8 @@ class Review(core_models.TimeStampedModel):
             + self.value
         ) / 6
         return round(avg, 2)
+
+    class Meta:
+        ordering = ("-created",)
 
     rating_average.short_description = "Avg."
